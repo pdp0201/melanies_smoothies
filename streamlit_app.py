3d742f7bb72abf177702e698708e531b4aec488d
pdp0201 <duplessispe@outlook.com>
2025-07-09 14:01:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,9 @@ if ingredients_list:
 
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
+
+#New secton to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), user_container_width=True)
